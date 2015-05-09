e53e02d8b401fabdf8231d5da19bdb104c51ddca
Amit Erandole <amit.erandole@gmail.com>
2015-05-09 09:06:35 +0530

    initial setup

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'susy', '~> 2.2.3'
+gem 'breakpoint', '~> 2.5.0'
+gem 'bitters', '~> 1.0.0'
---
+++ gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+  // Project Configuration
+  grunt.initConfig({
+      pkg: grunt.file.readJSON('package.json'),
+      watch: {
+          sass: {
+              files: ['src/*.scss'],
+              tasks: 'sass'
+          },
+      },
+      sass: {
+          dist: {
+              options: {
+                style: 'expanded',
+                sourcemap: 'auto',
+              },
+              files: {
+                  'css/site.css': 'src/site.scss'
+              }
+          }
+      },
+  });
+
+  //Load NPM tasks
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['sass', 'watch']);
+
+};
